6c6fd00672f3df6634b713f75512b3a66c6e4b32
liusining <siningdream@gmail.com>
2016-09-20 16:35:27 +0800

    beautify code

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -21,17 +21,17 @@ RSpec.describe CoursesController, type: :controller do
   end
 
   describe "GET show" do
-  it "assigns @course" do
-    course = FactoryGirl.create(:course)
-    get :show, :id => course.id
-    expect(assigns[:course]).to eq(course)
-  end
+    it "assigns @course" do
+      course = FactoryGirl.create(:course)
+      get :show, :id => course.id
+      expect(assigns[:course]).to eq(course)
+    end
 
-  it "render template" do
-    course = FactoryGirl.create(:course)
-    get :show, :id => course.id
-    expect(response).to render_template("show")
+    it "render template" do
+      course = FactoryGirl.create(:course)
+      get :show, :id => course.id
+      expect(response).to render_template("show")
+    end
   end
-end
 
 end
