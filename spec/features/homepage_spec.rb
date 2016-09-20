20ab730e2b39ebce242c94e66200fa6804b1b9bd
liusining <siningdream@gmail.com>
2016-09-20 23:22:00 +0800

    beautify code with PageObject

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 describe "homepage", :type => :feature do
 
   it 'welcomes the user' do
-    visit '/'
+    home_page.go
     expect(page).to have_content 'Welcome'
   end
+
+  private
+
+    def home_page
+      PageObjects::Pages::Home.new
+    end
+
 
 end
