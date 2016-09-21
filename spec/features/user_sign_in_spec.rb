39132f0b6cb232474bd7d24a5aa8982c5acd1357
liusining <siningdream@gmail.com>
2016-09-21 10:46:07 +0800

    to test sign in with good/bad examples

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 describe "user sign in", :type => :feature do
 
+
   before :each do
     User.create(:email => 'user@example.com', :password => 'password')
   end
 
-
-  it 'sign_in the user' do
-
+  scenario "good credentials" do
     new_session_page.sign_in 'user@example.com', 'password'
     expect(page).to have_content 'user@example.com'
   end
 
-  private
-
-  def new_session_page
-    home_page.go
-    navbar.sign_in
+  scenario "bad credentials" do
+    new_session_page.sign_in 'XXX@example.com', 'password'
+    expect(page).not_to have_content 'user@example.com'
   end
+
+
+  private
 
   def home_page
     PageObjects::Pages::Home.new
@@ -26,6 +26,11 @@ describe "user sign in", :type => :feature do
 
   def navbar
     PageObjects::Application::Navbar.new
+  end
+
+  def new_session_page
+    home_page.go
+    navbar.sign_in
   end
 
 end
