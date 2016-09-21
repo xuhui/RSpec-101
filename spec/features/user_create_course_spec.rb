86b1da1f2d1c6ac5f12bc83296dee1f1f0e768a7
liusining <siningdream@gmail.com>
2016-09-21 11:23:23 +0800

    to test user create course page with i18n to test text

--- spec/features/user_create_course_spec.rb
+++ spec/features/user_create_course_spec.rb
@@ -10,7 +10,7 @@ describe "user create course", :type => :feature do
 
   scenario "invalid" do
     course_form.create("", "")
-    expect(page).to have_text("can't be blank")
+    expect(page).to have_text t("courses.new.add_course")
   end
 
   private
